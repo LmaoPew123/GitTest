88e73d684e52cefd30ed56693faeff55c0b04f26
LmaoPewPew <asdfghjkl.98@gmx.de>
2021-01-27 09:00:23 +0100

    new Brnch, PeePeeSize function main.py

--- main.py
+++ main.py
@@ -6,3 +6,13 @@ def calc(x, y ):
     return x + y
 
 print(calc(x,y))
+
+nameInput = input()
+
+def peepeeSize(name):
+    if name == 'Lmao':
+        print('WoW such a Big Dick UwO')
+    else:
+        print('Nuh dawg,you aint got it homie')
+
+peepeeSize(nameInput)
